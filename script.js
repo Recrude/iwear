8095fd4785cc2be33833cdf69bfc9b06d643aac8
Jaeyeon Kim <70138657+Recrude@users.noreply.github.com>
2022-06-10 03:17:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function setBackground(images) {
+    setInterval(function() {
+        var index = Math.floor(Math.random() * (urls.length));
+        var target = document.getElementsByTagName("body")[0];
+        target.style.backgroundImage = "url(" + urls[index] + ")";
+
+    }, 2000);
+}
+
+var urls = [ 'images/img_01.jpg',
+    'images/img_02.jpg',
+    'images/img_03.jpg',
+    'images/img_04.jpg'
+    ];
+
+
+setBackground();
