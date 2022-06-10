1659f01036fae7eb551c5900968bdbd22a2a7505
Jaeyeon Kim <70138657+Recrude@users.noreply.github.com>
2022-06-10 11:09:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-
-
 function setBackground(images) {
     setInterval(function() {
         var index = Math.floor(Math.random() * (urls.length));
         var target = document.getElementsByTagName("body")[0];
         target.style.backgroundImage = "url(" + urls[index] + ")";
+    }, 2000);
+}
 
+function setBackgroundMobile(images) {
+    setInterval(function() {
+        var index = Math.floor(Math.random() * (urls.length));
+        var target = document.getElementsByClassName("bg")[0];
+        target.style.backgroundImage = "url(" + urls[index] + ")";
     }, 2000);
 }
 
@@ -17,3 +22,4 @@ var urls = [ 'images/img_01.jpg',
 
 
 setBackground();
+setBackgroundMobile();
